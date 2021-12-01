cbcde881a2d0d73bbd6485efd51ccacf6c211076
Lewis Mocha <mocha@lewis.mocha>
2021-12-01 14:18:16 +0300

    Added the input form and js file

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,8 @@
+const maleNames =["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"]
+
 function century(CC){
-    return ((CC/4) -2*CC-1);
+    return ((CC / 4) - 2* CC-1);
 }
 
 function year(YY){
@@ -9,3 +12,25 @@ function year(YY){
 function month (MM){
     return (26*(MM+1)/10);
 }
+var enterCentury  = Number(prompt("Which centruy were you born"))
+var enterYear  = Number(prompt("Which Year were you born"))
+var enterMonth  = Number(prompt("Which Month were you born"))
+var enterDate  = Number(prompt("Which Date were you born"))
+var gender = prompt("What is your gender?")
+
+const birthCentury = century(enterCentury)
+const birthYear = year(enterYear)
+const birthMonth = month(enterMonth)
+
+var calculationResults = (birthCentury + birthYear + birthMonth + enterDate) % 7
+const wholeNumber = calculationResults.toFixed(0);
+alert(wholeNumber)
+let result;
+if (gender == "male"){
+    result = maleNames[wholeNumber]
+}else if (gender == "female"){
+    result = femaleNames[wholeNumber]
+}else{
+    alert("Enter Correct genter")
+}
+alert(result)
